c045e7d085b6a4a259ef4255687c38153292673c
paulokow <31203062+paulokow@users.noreply.github.com>
2017-08-27 23:49:39 +0200

    Changed script to redirect error output

--- scan_pump.sh
+++ scan_pump.sh
@@ -3,5 +3,5 @@ export LOGFILE="PumpScan.log"
 export PYTHONPATH=./decoding-contour-next-link
 echo "SCRIPT>> Staring..." >> $LOGFILE
 date >> $LOGFILE
-./virtualenv/bin/python -u bg_data_export2.py >> $LOGFILE
+./virtualenv/bin/python -u bg_data_export2.py 2>&1 >> $LOGFILE
 echo "SCRIPT>> Done!" >> $LOGFILE
