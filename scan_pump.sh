ab1615f65f84c84258c96be843ddaed542c40903
paulokow <31203062+paulokow@users.noreply.github.com>
2017-08-30 06:13:04 +0200

    BUGFIX: redirecting stderr correctly

--- scan_pump.sh
+++ scan_pump.sh
@@ -3,5 +3,5 @@ export LOGFILE="PumpScan.log"
 export PYTHONPATH=./decoding-contour-next-link
 echo "SCRIPT>> Staring..." >> $LOGFILE
 date >> $LOGFILE
-./virtualenv/bin/python -u bg_data_export2.py 2>&1 >> $LOGFILE
+./virtualenv/bin/python -u bg_data_export2.py  >> $LOGFILE 2>&1
 echo "SCRIPT>> Done!" >> $LOGFILE
