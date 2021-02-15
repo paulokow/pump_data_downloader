8d6e014e6132c54854903db92a4fbfee08f7e37b
paulokow <31203062+paulokow@users.noreply.github.com>
2021-02-15 23:38:31 +0100

    IMPROVEMENT: no overloaded notifications in the evening

--- bg_data_export2.py
+++ bg_data_export2.py
@@ -189,6 +189,7 @@ class LatestActivity (object):
 
                 # calibration needed in the evening
                 if datetime.now().time() > time(23, 00) \
+                    and datetime.now().time() < time(23, 15) \
                     and (datetime.now() + timedelta(minutes=status.sensorCalibrationMinutesRemaining)).time() < time(7, 00) \
                     and status.sensorStatusValue != 0x00 :
                     print("Evening calibration needed.")
